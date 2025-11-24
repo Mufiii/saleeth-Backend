e97f73bb36f4c46c7d45f629e0823ef7adb151bb
mufiii <mufeesrahman9795@gmail.com>
2025-11-24 15:16:33 +0530

    db updated

--- BookReader/settings.py
+++ BookReader/settings.py
@@ -83,25 +83,19 @@ WSGI_APPLICATION = 'BookReader.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'saleethDb',          
-#         'USER': 'saleethuser',        
-#         'PASSWORD': 'MlYummYTJRV8jHJPzYPP',
-#         'HOST': 'saleethdb.cpuwqokyej82.eu-north-1.rds.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'saleethDb',          
+        'USER': 'saleethuser',        
+        'PASSWORD': 'MlYummYTJRV8jHJPzYPP',
+        'HOST': 'saleethdb.cpuwqokyej82.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
